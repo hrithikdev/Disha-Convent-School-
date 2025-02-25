a3840750a004d252537cbd674dca124392be9dca
Hrithik Pandey <hrithikpandey.dev@gmail.com>
2025-02-26 02:10:40 +0530

    added gallery1 file

---
+++ eventsfile.js
@@ -0,0 +1,17 @@
+events=[
+    {
+        name: "Science Exhibition",
+        img: ["./img/img1.jpg", "./img/img2.jpg", "./img/img3.jpg", "./img/img4.jpg"],
+        date: "18/11/2023"
+    },
+    {
+        name: "Raksha Bandhan",
+        img: ["./img/img5.jpg", "./img/img6.jpg", "./img/img7.jpg", "./img/img8.jpg"],
+        date: "18/11/2023"
+    },
+    {
+        name: "Independence Day",
+        img: ["./img/img9.jpg", "./img/img1.jpg", "./img/img2.jpg", "./img/img3.jpg"],
+        date: "18/11/2023"
+    }
+]
