4a5e4d85bf529fcdb7759e5442c77037e0686cfd
Hrithik Pandey <hrithikpandey.dev@gmail.com>
2025-02-27 01:01:35 +0530

    made gallery page dynamic

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Define Schema & Model
+const EventSchema = new mongoose.Schema({
+  name: String,
+  date: String,
+  images: [String],
+});
+const Event = mongoose.model("Event", EventSchema);
+
+// API Route to Fetch Events
+app.get("/events", async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- eventsfile.js
+++ eventsfile.js
@@ -1,17 +1,52 @@
-events=[
-    {
-        name: "Science Exhibition",
-        img: ["./img/img1.jpg", "./img/img2.jpg", "./img/img3.jpg", "./img/img4.jpg"],
-        date: "18/11/2023"
-    },
-    {
-        name: "Raksha Bandhan",
-        img: ["./img/img5.jpg", "./img/img6.jpg", "./img/img7.jpg", "./img/img8.jpg"],
-        date: "18/11/2023"
-    },
-    {
-        name: "Independence Day",
-        img: ["./img/img9.jpg", "./img/img1.jpg", "./img/img2.jpg", "./img/img3.jpg"],
-        date: "18/11/2023"
+const API_URL = "http://localhost:5000/events";
+
+async function fetchEvents() {
+    try {
+        const response = await fetch(API_URL);
+        const events = await response.json();
+        // console.log(events);
+        return events;
+    } catch (error) {
+        console.error("Error fetching events:", error);
+        return [];
     }
-]
+}
+
+fetchEvents();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// events=[
+//     {
+//         name: "Science Exhibition",
+//         img: ["./img/img1.jpg", "./img/img2.jpg", "./img/img3.jpg", "./img/img4.jpg"],
+//         date: "18/11/2023"
+//     },
+//     {
+//         name: "Raksha Bandhan",
+//         img: ["./img/img5.jpg", "./img/img6.jpg", "./img/img7.jpg", "./img/img8.jpg"],
+//         date: "18/11/2023"
+//     },
+//     {
+//         name: "Independence Day",
+//         img: ["./img/img9.jpg", "./img/img1.jpg", "./img/img2.jpg", "./img/img3.jpg"],
+//         date: "18/11/2023"
+//     }
+// ]
